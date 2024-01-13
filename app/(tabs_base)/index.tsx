8e7005544938a690ab616e501c37475137e88130
jeden1102 <bum123@o2.pl>
2024-01-13 21:06:08 +0100

    Init commit

--- app/(tabs_base)/index.tsx
+++ app/(tabs_base)/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
